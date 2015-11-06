56f575763f786a36d34534aef9ed806bd761e3e0
agrasagar <agrasagar@gmail.com>
2015-11-06 20:53:06 +0200

    header and footer added

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,7 +5,16 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
+      header: {
+          center: 'month,agendaFourDay' // buttons for switching between views
+      },
+      views: {
+          agendaFourDay: {
+              type: 'agenda',
+              duration: { days:   7 },
+              buttonText: '7 day'
+          }
+      }
+  });
 
 });
