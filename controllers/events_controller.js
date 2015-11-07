40eabaf9b1d71beca26e81e0f503528e94ac012f
Dinesh <dnes.elves@gmail.com>
2015-11-07 22:01:58 +0200

    integrated create, edit and delete functionalities with calendar

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -8,12 +8,60 @@ routing.index = function(req, res){
     console.log("get events");
 
     client.get("http://localhost:3000/events", function(data, response){
-      console.log("api call");
-      console.log(JSON.parse(data));
       var events = JSON.parse(data);
-      res.send(events);
+      var events_array = []
+
+      for(i in events){
+        var new_event = {}
+        new_event.id = events[i]._id;
+        new_event.title = events[i].name;
+        new_event.start = events[i].start_date;
+        new_event.description = events[i].description;
+      //  new_event.end = events[i].end_date;
+        new_event.allDay = false;
+        new_event.timezone = "Europe/Helsinki";
+        events_array.push(new_event);
+      }
+
+      res.send(events_array);
     });
 };
 
+routing.create = function(req, res){
+  var args = {
+    data: req.body,
+    headers:{"Content-Type": "application/json"}
+  };
+
+  client.post("http://localhost:3000/events", args, function(data, response){
+    var events = JSON.parse(data);
+    res.send(events);
+  });
+
+}
+
+
+routing.update = function(req, res){
+  var event = req.body;
+  var args = {
+    data: event,
+    headers:{"Content-Type": "application/json"}
+  };
+
+  client.put("http://localhost:3000/events/"+event._id , args, function(data, response){
+    var json = JSON.parse(data);
+    res.send(json);
+  });
+
+}
+
+routing.destroy = function(req, res) {
+  var id = req.params.id;
+  client.delete("http://localhost:3000/events/"+id, function(data, response){
+    var result = JSON.parse(data);
+
+    res.send(result);
+  });
+}
 
 module.exports = routing;
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,7 @@
-console.log("testttttttt");
 
 $(document).ready(function() {
 
+/*
     $( "#Create" ).click(function() {
         $("#box").fadeIn('slow');
         $("#dialog").fadeIn('slow');
@@ -26,20 +26,7 @@ $(document).ready(function() {
     $( "#Search" ).click(function() {
       $( this ).slideUp();
     });
-    //to get from the //http://localhost:3000/events
-    $.ajax({
-      url: "events",     //http://localhost:3000/events
-      method: "GET"
-    }).done(function(data){
-      console.log(data);
-      var events = [];
-      $.each(data, function(index, event){
-        var event = {};
-        event["title"] = data[index].name;
-        event["start"] = data[index].start_date;
-        event["description"] = data[index].description;
-        events[index] = event;
-      })
+*/
     // page is now ready, initialize the calendar...
       $('#calendar').fullCalendar({
 
@@ -64,27 +51,109 @@ $(document).ready(function() {
                 buttonText: 'agenda7day'
               }
             },
-            events:events
+            //url for fetching events
+            events: "events",
+            timezone: "Europe/Helsinki",
+            allDay: false,
+            eventClick: function( event, jsEvent, view ) {
+              showEvent(event, jsEvent, view);
+            },
+            dayClick: function(date, jsEvent, view) {
+              newEvent(date, jsEvent, view);
+            }
+
           });
-      });
+
+
+  $("#event_start_date").datepicker({
+    dateFormat: "yy-mm-dd"
+  });
+
+  $("#event_start_time").timepicker({
+    timeFormat: "H:i"
+  });
+  $("#event_save_btn").click(function(){
+    saveEvent();
+    return false;
+  });
+
+  $("#event_delete_btn").click(function(){
+    deleteEvent();
+    return false;
+  })
+
 });
-/*var getEvents = function(){
-  console.log("getEvents()");
+//---document ready block ends here
+
+var showEvent = function(calEvent, jsEvent, view){
+  $("#new_event_note").hide();
+  $("#action_type").val("edit");
+  $("#event_id").val(calEvent.id);
+  $("#event_name").val(calEvent.title);
+  $("#event_description").val(calEvent.description);
+  $("#event_start_date").datepicker("setDate", calEvent.start.toDate());
+  $("#event_start_time").timepicker("setTime", calEvent.start.toDate());
+
+}
+
+var newEvent = function(date, jsEvent, view){
+  $("#new_event_note").show();
+  $("#action_type").val("new");
+  $("#event_id").val("");
+  $("#event_name").val("");
+  $("#event_description").val("");
+  $("#event_start_date").datepicker("setDate", date.toDate());
+  $("#event_start_time").timepicker("setTime", date.toDate());
+};
+
+var saveEvent = function(){
+  var params = {};
+  var action_type = $("#action_type").val();
+  if(action_type == "edit"){ params._id = $("#event_id").val(); }
+  params.name = $("#event_name").val().trim();
+  params.description = $("#event_description").val().trim();
+  params.start_date = $("#event_start_date").val().trim();
+  params.start_time = $("#event_start_time").val().trim();
+  if(!params.name){
+    alert("Please enter event name!");
+    return;
+  }
+
+
+  if(action_type == "new"){
+    var url = "/events/";
+    var method = "POST";
+  } else {
+    var url = "/events/" + params._id;
+    var method = "PUT";
+  }
 
   $.ajax({
-    url: "events",     //http://localhost:3000/events
-    method: "GET"
-  }).done(function(data){
-    console.log(data);
-    var events = [];
-    $.each(data, function(index, event){
-      var event = {};
-      event["title"] = data[index].name;
-      event["start"] = data[index].start_date;
-      event["description"] = data[index].description;
-      events[index] = event;
-    })
-    console.log(events);
-    return events;
-});
-}*/
+    url: url,
+    method: method,
+    data: params,
+    async: false,
+    success: function(data){
+      console.log(data);
+      $('#calendar').fullCalendar('refetchEvents');
+      //$('#calendar').fullCalendar('rerenderEvents');
+    }
+  });
+
+}
+
+var deleteEvent = function(){
+  var action_type = $("#action_type").val();
+  if(action_type == "edit"){ var id = $("#event_id").val(); }
+  $.ajax({
+    url: "events/"+id,
+    method: "DELETE",
+    async: false,
+    success: function(data){
+      console.log(data);
+      $('#calendar').fullCalendar('refetchEvents');
+      //$('#calendar').fullCalendar('rerenderEvents');
+    }
+  });
+
+}
