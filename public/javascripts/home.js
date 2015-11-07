e09a851e8df2d98959c6baaa6289124b6df1eaf1
agrasagar <agrasagar@gmail.com>
2015-11-07 07:03:50 +0200

    html changes and create added

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,47 +2,89 @@ console.log("testttttttt");
 
 $(document).ready(function() {
 
-  var events = getEvents();
-  console.log("1111111111111111");
-  console.log(events);
+    $( "#Create" ).click(function() {
+        $("#box").fadeIn('slow');
+        $("#dialog").fadeIn('slow');
+    });
+    $('#submit').click(function(){
+      var name = $('input[name="firstName"]').val();
+      var description = $('input[name="description"]').val();
+      var start_date = $('input[name="start_date"]').val();
+      console.log(name);
+      console.log(description);
+      $('#box,#dialog').hide();
+    })
+    $('#cancel').click(function(){
+      $('#box,#dialog').hide();
+    })
+    $( "#Upadate" ).click(function() {
+      $( this ).slideUp();
+    });
+    $( "#Delete" ).click(function() {
+      $( this ).slideUp();
+    });
+    $( "#Search" ).click(function() {
+      $( this ).slideUp();
+    });
+    //to get from the //http://localhost:3000/events
+    $.ajax({
+      url: "events",     //http://localhost:3000/events
+      method: "GET"
+    }).done(function(data){
+      console.log(data);
+      var events = [];
+      $.each(data, function(index, event){
+        var event = {};
+        event["title"] = data[index].name;
+        event["start"] = data[index].start_date;
+        event["description"] = data[index].description;
+        events[index] = event;
+      })
+    // page is now ready, initialize the calendar...
+      $('#calendar').fullCalendar({
 
-
-    $('#calendar').fullCalendar({
-      header: {
-          center: 'month,agendaFourDay' // buttons for switching between views
-      },
-      views: {
-          agendaFourDay: {
-              type: 'agenda',
-              duration: { days:   7 },
-              buttonText: '7 day'
-          }
-      }
-  });
-
+        header: {
+          left: 'prev,next',
+          center: 'title',
+          right: 'month,agenda7Day,dayview' // buttons for switching between views
+          },
+          views: {
+            month:{
+              dragOpacity: {
+                month: .2,
+                'default': .5
+              }
+            },
+          dayview:{
+              type: 'basic'
+          },
+          agenda7Day: {
+                type: 'agenda',
+                duration: { days:   7 },
+                buttonText: 'agenda7day'
+              }
+            },
+            events:events
+          });
+      });
 });
-
-
-
-var getEvents = function(){
+/*var getEvents = function(){
   console.log("getEvents()");
 
   $.ajax({
-    url: "events",
+    url: "events",     //http://localhost:3000/events
     method: "GET"
   }).done(function(data){
     console.log(data);
     var events = [];
     $.each(data, function(index, event){
       var event = {};
-      event["title"] = event.name;
-      event["start"] = event.start_date;
-      event["description"] = event.description;
+      event["title"] = data[index].name;
+      event["start"] = data[index].start_date;
+      event["description"] = data[index].description;
       events[index] = event;
     })
-
+    console.log(events);
     return events;
-
-  });
-
-}
+});
+}*/
